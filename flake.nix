05688149d83caab78a7eb83f0aef31eab381f931
Random Dude <randomdude16671@proton.me>
2025-10-08 20:15:59 +0530

    init

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Super minimal font management flake (NixOS only)";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    {
+      nixosModules = rec {
+        fontman = ./modules/nixos;
+        default = fontman;
+      };
+    };
+}
---
+++ nixos/fonts.nix
@@ -0,0 +1,100 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkPackageOption
+    mkEnableOption
+    mkOption
+    types
+    ;
+  cfg = config.fontman;
+
+  mkFontOptions =
+    {
+      name,
+      displayName,
+      package,
+    }:
+    {
+      package = mkPackageOption pkgs package { } // {
+        description = "Package providing ${displayName} font";
+      };
+
+      name = mkOption {
+        type = types.str;
+        description = "Name of ${displayName} font";
+        default = name;
+      };
+    };
+in
+{
+  options.fontman = {
+    enable = mkEnableOption "Enable font management through fontman";
+    fonts = mkOption {
+      description = "actual font settings";
+      type = types.submodule {
+        options = {
+          serif = mkFontOptions {
+            displayName = "Serif";
+            name = "DejaVu Serif";
+            package = "dejavu_fonts";
+          };
+          sansSerif = mkFontOptions {
+            displayName = "Sans Serif";
+            name = "Adwaita Sans";
+            package = "adwaita-fonts";
+          };
+          monospace = mkFontOptions {
+            displayName = "Monospace";
+            name = "IosevkaTerm Nerd Font";
+            package = "nerd-fonts.iosevka-term";
+          };
+          emoji = mkFontOptions {
+            displayName = "Emoji";
+            fontName = "Noto Color Emoji";
+            package = "noto-fonts-color-emoji";
+          };
+        };
+      };
+    };
+    extraPackages = mkOption {
+      description = ''
+        List of all the font packages that will be installed
+      '';
+      type = types.listOf types.package;
+    };
+
+    config = lib.mkIf cfg.enable {
+      fonts.packages =
+        let
+          inherit (cfg) extraPackages;
+          inherit (cfg.fonts)
+            monospace
+            serif
+            sansSerif
+            emoji
+            ;
+        in
+        [
+          monospace.package
+          serif.package
+          sansSerif.package
+          emoji.package
+        ]
+        ++ extraPackages;
+      fonts.fontconfig = {
+        enable = cfg.enable;
+        defaultFonts = lib.getAttrs [
+          "monospace"
+          "serif"
+          "sansSerif"
+          "emoji"
+        ] (family: [ cfg.fonts.${family}.name ]);
+      };
+    };
+  };
+}
